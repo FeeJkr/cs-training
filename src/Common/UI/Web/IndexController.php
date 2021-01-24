4c4c6dd594ee426d3b7d49780b03110ab35cdc18
Sergii Siryi <sergii.siryi97@gmail.com>
2021-01-24 13:23:23 +0100

    add site index page

--- src/Common/UI/Web/IndexController.php
+++ src/Common/UI/Web/IndexController.php
@@ -3,13 +3,23 @@ declare(strict_types=1);
 
 namespace App\Common\UI\Web;
 
+use App\Faceit\Application\FaceitService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 final class IndexController extends AbstractController
 {
+    private FaceitService $faceitService;
+
+    public function __construct(FaceitService $faceitService)
+    {
+        $this->faceitService = $faceitService;
+    }
+
     public function dashboard(): Response
     {
-        return $this->redirectToRoute('training.dashboard');
+        return $this->render('common/dashboard.html.twig', [
+            'players' => $this->faceitService->getAllPlayersNicknames(),
+        ]);
     }
 }
